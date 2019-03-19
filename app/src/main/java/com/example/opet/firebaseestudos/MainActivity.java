9295d5c78f671d19aba954701a76bcd36508266c
opet <opet@Academico.edu>
2019-03-18 21:19:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/opet/firebaseestudos/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.opet.firebaseestudos;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editLogin, editSenha;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editLogin = findViewById(R.id.editLogin);
+        editSenha = findViewById(R.id.editSenha);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null) {
+            Toast.makeText(this, "Logado", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Usuário não logado", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void singnIn(View view){
+        String login = editLogin.getText().toString();
+        String senha = editSenha.getText().toString();
+        firebaseAuth.signInWithEmailAndPassword(login,senha).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this,"Logado com sucesso", Toast.LENGTH_SHORT).show();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,"Erro ao logar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void singin(View view) {
+    }
+}
